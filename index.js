d46148326042ff84e6dddcdec8d6eba02df71609
Ephraim Djeket <djeketefra10@hotmail.com>
2023-05-24 15:57:57 +0200

    edits

--- index.js
+++ index.js
@@ -10,20 +10,26 @@ fetch(BASEURL + ALLPRODUCTS)
 .then(data => {
     spinner.classList.remove('spinner-visible');
     const CONTAINER = document.querySelector(".grid-container");
-    CONTAINER.innerHTML =  `<div class="jacket-box">
-                            <a href="Jackets.html"><img src="${data[1].image}" class="jackethome-img"></a>
+    CONTAINER.innerHTML =  `
+                            <a href="Jackets.html">
+                            <div class="jacket-box">
+                            <img src="${data[1].image}" class="jackethome-img">
                             <h3>${data[1].title}</h3>
                             <p>${data[1].description}</p>
                             </div>
+                            </a>
+                            <a href="Jackets.html">
                             <div class="jacket-box">
-                            <a href="Jackets.html"><img src="${data[2].image}" alt="Soft-Shell Hi V Jacket" class="jackethome-img"></a>
+                            <img src="${data[2].image}" alt="Soft-Shell Hi V Jacket" class="jackethome-img">
                             <h3>${data[2].title}</h3>
                             <p>${data[2].description}</p></div>
+                            </a>
+                            <a href="Jackets.html">
                             <div class="jacket-box">
-                            <a href="Jackets.html"><img src="${data[0].image}" alt="First Watch AC 1100 Flotation Coat" class="jackethome-img"></a>
+                            <img src="${data[0].image}" alt="First Watch AC 1100 Flotation Coat" class="jackethome-img">
                             <h3>${data[0].title}</h3>
                             <p>${data[0].description}</p>
-                            </div>`
+                            </div></a>`
 })
 .catch(error => { console.log('An error occurred:', error);
 spinner.classList.remove('spinner-visible');
