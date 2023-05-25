cb3cda268391218d359916d926a00fd3193b14fd
Ephraim Djeket <djeketefra10@hotmail.com>
2023-05-25 15:43:40 +0200

    edits

--- index.js
+++ index.js
@@ -13,23 +13,31 @@ fetch(BASEURL + ALLPRODUCTS)
     CONTAINER.innerHTML =  `
                             <a href="Jackets.html">
                             <div class="jacket-box">
+                            <div class="cardContent">
                             <img src="${data[1].image}" class="jackethome-img">
                             <h3>${data[1].title}</h3>
                             <p>${data[1].description}</p>
                             </div>
+                            </div>
                             </a>
                             <a href="Jackets.html">
                             <div class="jacket-box">
+                            <div class="cardContent">
                             <img src="${data[2].image}" alt="Soft-Shell Hi V Jacket" class="jackethome-img">
                             <h3>${data[2].title}</h3>
-                            <p>${data[2].description}</p></div>
+                            <p>${data[2].description}</p>
+                            </div>
+                            </div>
                             </a>
                             <a href="Jackets.html">
                             <div class="jacket-box">
+                            <div class="cardContent">
                             <img src="${data[0].image}" alt="First Watch AC 1100 Flotation Coat" class="jackethome-img">
                             <h3>${data[0].title}</h3>
                             <p>${data[0].description}</p>
-                            </div></a>`
+                            </div>
+                            </div>
+                            </a>`
 })
 .catch(error => { console.log('An error occurred:', error);
 spinner.classList.remove('spinner-visible');
